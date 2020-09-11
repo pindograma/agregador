09b41f538b98a09a7c196c9458f9cb0f7b3bc051
Daniel Ferreira <bnmvco@gmail.com>
2020-09-10 22:41:16 -0300

    add preprocessor script
    Shiny has to launch online whenever it's gone idle, so it's important
    that we get to launch fast. However, computing Blank/Null votes on
    startup was taking quite a bit of time.
    
    We solve that by doing that on a preprocessing script, and just loading
    the Shiny app with that preprocessed data.

--- agregador/app.R
+++ agregador/app.R
@@ -16,38 +16,8 @@ Rcpp::sourceCpp('polling_average.cpp')
 source('theme.R')
 source('constants.R')
 
-early_polls = read_csv('data/early_polls.csv')
-late_polls = read_csv('data/late_polls.csv')
 early_polls_2020 = read_csv('data/early_polls_2020.csv')
-
-polls = bind_rows(early_polls, late_polls, early_polls_2020) %>%
-  mutate(imputed_ci = case_when(
-    is.na(confidence_interval_final) ~ '?',
-    confidence_interval_final == 100 ~ '?',
-    confidence_interval_final < 80 ~ '?',
-    T ~ paste0(format(round(confidence_interval_final, 1), decimal.mark = ','), '%')
-  )) %>%
-  mutate(imputed_error = case_when(
-    is.na(error_final) ~ '?',
-    error_final > 10 ~ '?',
-    error_final == 0 ~ '?',
-    T ~ paste0(format(round(error_final, 1), decimal.mark = ','), 'p.p.')
-  )) %>%
-  select(year, NR_IDENTIFICACAO_PESQUISA, SG_UE, polled_UE, CD_CARGO, estimulada,
-         result, DT_INICIO_PESQUISA, DT_FIM_PESQUISA, self_hired, turno,
-         NUMERO_CANDIDATO, company_id, pretty_name, first_round_date, second_round_date,
-         candidate_registry_date, partisan, hirer, imputed_ci, imputed_error,
-         undecided) %>%
-  group_split(year, CD_CARGO, SG_UE, turno, NR_IDENTIFICACAO_PESQUISA, company_id, estimulada) %>%
-  map_dfr(function(x) {
-    r = rbind(x, x[1,])
-    
-    r[nrow(r), 'result'] = first(r$undecided)
-    r[nrow(r), 'NUMERO_CANDIDATO'] = 99
-    
-    r
-  }) %>%
-  ungroup()
+polls = read_csv('data/preprocessed_polls.csv')
 
 cities = read_csv('data/cities.csv') %>%
   expand_grid(year = c(2012, 2014, 2016, 2018, 2020)) %>%
