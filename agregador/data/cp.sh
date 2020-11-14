56eca01d9f674835c9410d05cc9ae8f5ea3f6554
Daniel Ferreira <bnmvco@gmail.com>
2020-11-14 17:25:02 -0300

    update polling data

---
+++ agregador/data/cp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cp ../../../polling/output/early_polls_2020_2.csv .
+cp ../../../polling/output/recent_polls_2020.csv .
+cp ../../../polling/output/pindograma_candlist_2020.csv .
+cp ../../../polling/output/cities_2020.csv .
